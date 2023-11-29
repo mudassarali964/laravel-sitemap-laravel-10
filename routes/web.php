84486d08f0255b0e652d4476d2dda77842170459
mudassarali964 <55048197+mudassarali964@users.noreply.github.com>
2023-11-29 14:57:41 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/post/{slug}', [App\Http\Controllers\PostController::class, 'index'])->name('post');
